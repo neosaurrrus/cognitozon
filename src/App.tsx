2c1e20db9bd67329d87b76b3aed49cab2db5b1a4
Lukie Kang <lukiek@gmail.com>
2023-11-16 14:45:20 +0000

    feat: add useFetch hook, add basic test in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useFetch } from "./hooks/useFetch"
+
+// TODO: Find a better place for this:
+const getProductsUrl = 'https://s3.eu-west-2.amazonaws.com/techassessment.cognitoedu.org/products.json' //
+
+
 function App() {
+
+  const { fetchedData, isLoading, error} = useFetch(getProductsUrl)
+  
+  console.log(fetchedData)
+  console.log(isLoading + ':isLoading')
+  console.log(error + ':error')
+  
   return (
     <>
-      <h1 className="text-blue-200">Test</h1>
+      
     </>
   )
 }
