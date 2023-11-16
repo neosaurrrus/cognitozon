6f12a4fd40cc40266aa9b84c916e1de12c312019
Lukie Kang <lukiek@gmail.com>
2023-11-16 22:49:10 +0000

    feat: add placeholder header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+
+function Header() {
+
+  return (
+    <nav className='w-full bg-blue-700 h-16 flex justify-between items-center px-4'>
+      <li>Logo</li>
+      <li>Cognitozon</li>
+      <li>Basket</li>
+    </nav>
+  )
+}
+
+export default Header
