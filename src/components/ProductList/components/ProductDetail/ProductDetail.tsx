dbc534f2853f1363bbe9ff090c766f0a3a33e641
Lukie Kang <lukiek@gmail.com>
2023-11-20 15:10:40 +0000

    feat: add product detail component

---
+++ src/components/ProductList/components/ProductDetail/ProductDetail.tsx
@@ -0,0 +1,40 @@
+import { BasketType, ProductType } from '@/types'
+import ButtonBasketAdd from '@/components/ProductList/components/ButtonBasketAdd/ButtonBasketAdd'
+import { useContext } from 'react'
+import { BasketContext } from '@/App'
+
+interface Props {
+  selectedProduct: ProductType
+  setSelectedProductFn: (product: ProductType | null) => void
+  closeDialogFn: () => void
+}
+
+const ProductDetail = ({selectedProduct, setSelectedProductFn, closeDialogFn}: Props) => {
+    const basket: BasketType = useContext(BasketContext).basket
+    const {name, price, description} = selectedProduct as ProductType
+    const basketCount = basket[name] || 0
+    
+    const handleCloseClick = () => {
+        setSelectedProductFn(null)
+        closeDialogFn()
+    }
+    return (
+        <>
+          <h2 className='text-2xl mb-4'>{name}</h2>
+          <p className='mb-4'>{description}</p>
+          <p className='text-2xl mb-4'>£{price}</p>
+          <p>{basketCount} in basket</p>   
+          <div className='flex flex-col mt-8'>
+            <ButtonBasketAdd name={name} />
+            <button 
+                autoFocus
+                onClick={handleCloseClick}
+                className='bg-cognito-gray text-gray-100 hover:scale-110 duration-300 shadow-md rounded p-2 mt-4'>Close
+            </button>
+          </div>
+        </>
+    )
+  }
+
+
+export default ProductDetail
