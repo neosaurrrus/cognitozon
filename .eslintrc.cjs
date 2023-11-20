56311c61822c8c587ff62192b9ec83e354f903f1
Lukie Kang <lukiek@gmail.com>
2023-11-20 10:27:24 +0000

    revert: clearing up eslint config file causing conflicts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,15 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'react-app',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/no-unused-vars': ["error", { "argsIgnorePattern": "^_" }] // Allows use of _ convention for unused variables
+    "react-refresh/only-export-components": ["warn",{ allowConstantExport: true }],
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }], // Allows use of _ convention for unused variables
   },
-}
+};
