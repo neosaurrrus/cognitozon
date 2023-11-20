597fdd1975d122d1f739f6577c3e60a36f9027fb
Lukie Kang <lukiek@gmail.com>
2023-11-20 15:11:18 +0000

    refactor: move add to basket button to its own component as its used in two components

---
+++ src/components/ProductList/components/ButtonBasketAdd/ButtonBasketAdd.tsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { BasketType } from "@/types"
+import { BasketContext } from "@/App"
+
+interface Props {
+    name: string
+}
+
+const ButtonBasketAdd = ({name}: Props) => {
+    const basket: BasketType = useContext(BasketContext).basket
+    const setBasketFn: (basket: BasketType) => void = useContext(BasketContext).setBasket
+
+    const handleAddToBasketClick = () => {
+        if (basket[name]) {
+          setBasketFn({...basket, [name]: basket[name] + 1})
+        } else {
+          setBasketFn({...basket, [name]: 1})
+        }
+        localStorage.setItem("basket", JSON.stringify(basket)); // TODO: Would have this logic in its own hook in a real app. (Though I'd probably use a DB instead of local storage in that case)
+    }
+
+    return (
+        <button 
+           onClick={handleAddToBasketClick}
+           className='bg-cognito-blue hover:scale-110 font-bold text-gray-100 py-2 px-4 rounded duration-300'>
+           <img src='src/assets/basket-add.svg' className='h-4 w-4 inline-block mr-2' alt='basket' /> <span>Add</span>
+       </button>
+    );
+};
+
+export default ButtonBasketAdd;
--- src/components/ProductList/components/ProductListItem/ProductListItem.tsx
+++ src/components/ProductList/components/ProductListItem/ProductListItem.tsx
@@ -1,25 +1,17 @@
-import { useContext } from "react"
-import { BasketType, ProductType } from "@/types"
-import { BasketContext } from "@/App"
+import { ProductType } from '@/types'
+import ButtonBasketAdd from '../ButtonBasketAdd/ButtonBasketAdd'
 
 interface Props {
   product: ProductType
+  setSelectedProductFn: (product: ProductType) => void
 }
 
-function ProductListItem({product}: Props) {
-  const basket: BasketType = useContext(BasketContext).basket
-  const setBasketFn: (basket: BasketType) => void = useContext(BasketContext).setBasket
-
+function ProductListItem({product, setSelectedProductFn}: Props) {
+  
   const {name, price} = product
   
-  const handleAddToBasketClick = () => {
-    if (basket[name]) {
-      setBasketFn({...basket, [name]: basket[name] + 1})
-    } else {
-      setBasketFn({...basket, [name]: 1})
-    }
-
-    localStorage.setItem("basket", JSON.stringify(basket)); // Probably would have this logic in its own hook in a real app.
+  const handleDetailsClick = () => {
+    setSelectedProductFn(product)
   }
 
   return (
@@ -28,13 +20,11 @@ function ProductListItem({product}: Props) {
         <div>
           <span>£{price}</span> 
           <button
-            className='bg-cognito-gray hover:scale-110 font-bold text-gray-100 py-2 px-4 ml-4 rounded duration-300'
-          >Details</button>
-          <button 
-            onClick={handleAddToBasketClick}
-            className='bg-cognito-blue hover:scale-110 font-bold text-gray-100 py-2 px-4 ml-4 rounded duration-300'>
-              <img src='src/assets/basket-add.svg' className='h-4 w-4 inline-block mr-2' alt='basket' /> <span>Add</span>
+            onClick={handleDetailsClick}
+            className='bg-cognito-gray hover:scale-110 font-bold text-gray-100 py-2 px-4 mx-4 rounded duration-300'
+          >Details
           </button>
+          <ButtonBasketAdd name={name} />
         </div>
     </li>
   )
