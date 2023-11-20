086e3e0972686fa7f74cf8975295a611b6d8f5ca
Lukie Kang <lukiek@gmail.com>
2023-11-20 15:12:34 +0000

    refactor: move dialog logic to its own hook

---
+++ src/hooks/useDialog.ts
@@ -0,0 +1,24 @@
+import {useRef} from 'react';
+
+const useDialog = () => {
+    const dialogRef = useRef(null)
+
+    const show = () => {
+        if (dialogRef.current) {
+            // We don't technically know what the ref could be, so we cast it to an HTMLDialogElement. I try to avoid doing this
+            const dialogElement = dialogRef.current as unknown as HTMLDialogElement
+            dialogElement.showModal()
+        }
+    }
+
+    const close = () => {
+        if (dialogRef.current) {
+            const dialogElement = dialogRef.current as unknown as HTMLDialogElement
+            dialogElement.close()
+        }
+    }
+
+    return {dialogRef, show, close}
+}
+
+export default useDialog
