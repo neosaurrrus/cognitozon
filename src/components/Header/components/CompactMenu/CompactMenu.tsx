15c86430385a31592a7d672531e30ca625d61a84
Lukie Kang <lukiek@gmail.com>
2023-11-23 21:58:27 +0000

    docs Update CompactMenu with comment

--- src/components/Header/components/CompactMenu/CompactMenu.tsx
+++ src/components/Header/components/CompactMenu/CompactMenu.tsx
@@ -1,7 +1,8 @@
+// This componet exists as a placeholder for a proper hamburger menu but thought it was beyond scope of the assessment, especially since the options don't do anything anyhow.
 const CompactMenu = () => (
     <select className="border bg-cognito-blue dark:bg-slate-800/25 text-lg text-gray-100 p-2 rounded shadow-md"
         onChange={(e) => {
-            window.location.href = e.target.value // This is not what i'd do in a real app!
+            window.location.href = e.target.value // This is definately not what i'd do in a real react app!
         }}
         value={window.location.pathname}
     >
